1c6a649e4487cd58b3ae04142322ca6b7a3ddbf6
Hector Martinez <kolrinah@gmail.com>
2017-05-07 11:53:31 -0400

    English Version

---
+++ contact/index.php
@@ -0,0 +1,45 @@
+<!DOCTYPE html>
+<html lang="en">
+ <head>
+    <title>Fit n Fresh Concessions</title>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="../img/fitnfreshico.png">
+    <link rel="shortcut icon" type="image/x-icon" href="../img/fitnfreshico.png">
+    <link rel="stylesheet" href="../css/bootstrap.min.css" type="text/css" />
+    <link rel="stylesheet" href="../css/fitnfresh.css" type="text/css" />
+    <link href="https://fonts.googleapis.com/css?family=Montserrat|Lato|Patrick+Hand|Annie+Use+Your+Telescope|Stalemate" rel="stylesheet" type="text/css">
+    <link href="https://fonts.googleapis.com/icon?family=Material+Icons" rel="stylesheet">
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+ </head>
+
+ <body style="text-align: center">
+
+    <div style="width: 100%;">
+        <a href="../" style="cursor: pointer" title="GO BACK!">
+            <img alt="Fit-n-Fresh Concessions" src="../img/fitnfreshlogo.png" >
+        </a>
+    </div>
+
+    <?php
+
+        if ($_POST){
+            $msg = $_POST['message'];
+            $email = $_POST['email'];
+            $to = "fitnfreshconcessions@gmail.com";
+            $name = $_POST['name'];
+            $subject = "Contact from: $name <$email>";
+            $headers = "From: no-reply@fitnfreshconcessions.com" . "\r\n" .
+                       "CC: $email \r\n";
+            // use wordwrap() if lines are longer than 70 characters
+            $msg = wordwrap($msg,70);
+
+            // send email
+            mail($to, $subject, $msg, $headers);
+            echo '<h2>Message Successfully Sent!</h2>';
+            echo "<h2>We'll get back to you within 24 hours.</h2>";
+        }
+        unset($_POST);
+    ?>
+ </body>
+</html>
